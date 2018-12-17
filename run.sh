d3b5e182e29bb0fb1ade980bdca8204029d01759
Anton Bracke <anton.bracke.com@gmail.com>
2018-12-18 00:55:59 +0100

    docker run script removed network and added port

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 docker run -td \
-	--name ledwall \
-	--restart always \
-	--network ju60_bridge \
-	-v $DIR/app:/app \
-	ledwall
+  --name ledwall \
+  --restart always \
+  -p 8080:80 \
+  -v $DIR/app:/app \
+  ledwall
