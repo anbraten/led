6cfd4626ff923f50041852c436abf9b461cdee05
Anton Bracke <anton.bracke.com@gmail.com>
2020-01-05 18:06:04 +0100

    fixed startup

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const matix = require('./matrix');
 const socket = require('./socket');
+const scripts = require('./scripts');
 const log = require('./log');
 
+const MATRIX_X = process.env.MATRIX_X || 10;
+const MATRIX_Y = process.env.MATRIX_Y || 10;
+
 (() => {
-  matix.init({ x: 10, y: 10 });
-  socket.init({ matrix });
+  matix.init({ x: MATRIX_X, y: MATRIX_Y });
+  scripts.init();
+  socket.init();
 
   log('Server started.');
 })();
